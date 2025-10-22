bab33867dc0d084cf4daa5d35d3b35aefa5d1f1c
ScryptSculptor <80274312+Arafaa199@users.noreply.github.com>
2025-10-22 21:27:11 +0400

    Update htb_scan.sh

--- htb_scan.sh
+++ htb_scan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if two arguments are passed
 if [ "$#" -ne 2 ]; then
    echo "Usage: $0 <box_name> <box_ip>"
    exit 1
@@ -11,21 +10,17 @@ box_ip="$2"
 
 # base dir for storing boxes
 base_dir="$HOME/HTB"
-
-# ensure base directory exists
 mkdir -p "$base_dir"
 
-# Create a directory named after the box within the base directory
 box_dir="$base_dir/$box_name"
 mkdir -p "$box_dir"
 
 box_file="$box_dir/$box_name.txt"
 touch "$box_file"
 
-# Run a full port scan to find open ports
+# a proper nmap scan and some slicing to get the ports for another round of nmap
 ports=$(nmap -Pn -p- --min-rate=1000 -T4 "$box_ip" | grep '^[0-9]' | cut -d '/' -f 1 | tr '\n' ',' | sed 's/,$//')
 
-# Check if any ports are open
 if [ -z "$ports" ]; then
    echo "No open ports found."
    exit 1
